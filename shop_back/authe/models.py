d630d4bf4b02627be3293f68470b24f29bb8e931
Alik <@Alik>
2020-04-24 22:39:34 +0600

    server settings

--- shop_back/authe/models.py
+++ shop_back/authe/models.py
@@ -14,11 +14,9 @@ class Profile(models.Model):
 
 
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
+def create_user_token(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
         Token.objects.create(user=instance)
-        UserPersonalCart.objects.create(owner=instance)
 
 
 @receiver(post_save, sender=User)

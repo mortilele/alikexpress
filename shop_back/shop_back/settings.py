6b5b5aade197583c5e25c01d824373ee838de36c
Alik <@Alik>
2020-04-24 22:54:10 +0600

    added ppostgres database

--- shop_back/shop_back/settings.py
+++ shop_back/shop_back/settings.py
@@ -92,12 +92,24 @@ WSGI_APPLICATION = 'shop_back.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'alikexpress',
+            'USER': 'alik',
+            'PASSWORD': 'aliktestpass',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 
 # Password validation
@@ -136,7 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 BACKEND_URL = "http://localhost:8000"
 
 MEDIA_URL = '/media/'

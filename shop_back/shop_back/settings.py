f542228cacccd50f9d4c15a3536e2f13a022f4a0
Alik <@Alik>
2020-04-26 12:47:17 +0600

    change backend url

--- shop_back/shop_back/settings.py
+++ shop_back/shop_back/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-BACKEND_URL = "http://api.ocenika.com"
+BACKEND_URL = "http://api.alikexpress.solf.io"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
